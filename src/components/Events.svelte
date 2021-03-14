ff245b63eda28018f742898b20901edd901725b1
maeta187 <nexus18789@gmail.com>
2021-03-14 13:16:45 +0900

    イベント修飾子を使用した実装

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -6,6 +6,10 @@
   //   axis.x = event.clientX;
   //   axis.y = event.clientY;
   // };
+
+  const handleClick = () => {
+    alert("no more alerts");
+  };
 </script>
 
 <!-- HTML要素内に直接書いて実行するパターン -->
@@ -14,6 +18,12 @@
   on:mousemove={(e) => (axis = { x: e.clientX, y: e.clientY })}
 >
   The mouse position is {axis.x} x {axis.y}
+</div>
+
+<!-- 1度実行するとリロードしない限り実行されない -->
+<!-- 他にもイベント修飾子はあるのでドキュメント参考（https://svelte.dev/tutorial/event-modifiers） -->
+<div class="handle-click-area">
+  <button on:click|once={handleClick}> Click me </button>
 </div>
 
 <!-- イベント発火させ、 script 内で実行するパターン -->
@@ -29,5 +39,9 @@
     padding: 100px 0;
     background: #bbf5ff;
     margin: 0 auto;
+  }
+
+  .handle-click-area {
+    margin: 30px 0;
   }
 </style>
